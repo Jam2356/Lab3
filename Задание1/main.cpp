42e70f8a3a444fa3502361f37c7ff542ae57e284
Jam2356 <jam2000.ru@gmail.com>
2022-12-19 23:40:37 +0300

    Upload_Lab3_1

--- Задание1/main.cpp
+++ Задание1/main.cpp
@@ -64,66 +64,6 @@ struct KeyAB_fixture {
         delete pointer;
     }
 };
-SUITE(EncryptTest)
-{
-    TEST_FIXTURE(KeyAB_fixture, LargeLetters) {
-        wstring open_text = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        wstring result_correct = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ";
-        CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text));
-    }
-    TEST_FIXTURE(KeyAB_fixture, SmallLetters) {
-        wstring open_text = L"абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        wstring result_correct = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ";
-        CHECK_EQUAL(true, result_correct == pointer->encrypt(open_text));
-    }
-    TEST_FIXTURE(KeyAB_fixture,NumberInText ) {
-        wstring open_text = L"азбука3256";
-        CHECK_THROW(pointer->encrypt(open_text),cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture,TextWithSpecialSymbol) {
-        wstring open_text = L"азбука/буква";
-        CHECK_THROW(pointer->encrypt(open_text),cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture,TextWithASpace ) {
-        wstring open_text = L"азбука буква";
-        CHECK_THROW(pointer->encrypt(open_text),cipher_error);
-    }
-
-}
-SUITE(DecryptTest)
-{
-    TEST_FIXTURE(KeyAB_fixture, LargeLetters) {
-        wstring cipher_text = L"АГВЕДЖЁИЗКЙМЛОНРПТСФУЦХШЧЪЩЬЫЮЭАЯ";
-        wstring result_correct = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));
-    }
-    TEST_FIXTURE(KeyAB_fixture, Smallletters) {
-        wstring cipher_text = L"агведжёизкймлонрптсфуцхшчъщьыюэая";
-        wstring result_correct = L"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));
-    }
-    TEST_FIXTURE(KeyAB_fixture,LargeAndSmallLetters ) {
-        wstring cipher_text = L"ПРиооИППП";
-        wstring result_correct = L"ПРИМААПЛДД";
-        CHECK_EQUAL(true, result_correct == pointer->decrypt(cipher_text));
-    }
-    TEST_FIXTURE(KeyAB_fixture, EmptyText) {
-        wstring cipher_text = L"";
-        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture,TextWithNumber) {
-        wstring cipher_text = L"вавау324жилиман";
-        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture,TextWithSymbol) {
-        wstring cipher_text = L"азбука/буква";
-        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
-    }
-    TEST_FIXTURE(KeyAB_fixture,TextWithASpace ) {
-        wstring cipher_text = L"азбука буква";
-        CHECK_THROW(pointer->decrypt(cipher_text),cipher_error);
-    }
-}
 int main()
 {
     return UnitTest::RunAllTests();
